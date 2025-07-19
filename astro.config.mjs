d79940854dfc29bbf4cf29db9b9766e7f8e2c2f1
Anirban Sikdar <anirban9293@gmail.com>
2025-07-19 23:15:14 +0530

    fix: set base path in astro.config.mjs for correct routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
     site: "https://anirban-1009.github.io/",
-    base: "",
+    base: "/",
     integrations: [tailwind(), icon(), mdx()],
     output: "server",
     adapter: vercel({
